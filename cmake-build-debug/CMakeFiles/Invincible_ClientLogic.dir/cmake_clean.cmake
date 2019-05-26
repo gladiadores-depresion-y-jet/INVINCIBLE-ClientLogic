f6a0c710a1ccc597b5942640b667d0e94003e0f3
David <david.azoherra@gmail.com>
2019-05-25 20:38:31 -0600

    Commit inicial con bibliotecas necesarias

---
+++ cmake-build-debug/CMakeFiles/Invincible_ClientLogic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Invincible_ClientLogic.dir/main.cpp.o"
+  "Invincible_ClientLogic.pdb"
+  "Invincible_ClientLogic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Invincible_ClientLogic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <pistache/net.h>
+#include <nlohmann/json.hpp>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
